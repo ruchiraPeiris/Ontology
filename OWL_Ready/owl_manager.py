705d733767f55909c9d49e0c810a6b8970cb4499
madushajg <madushajg@gmail.com>
2018-08-12 01:55:06 +0530

    Retreiving ontology data from owlready2

--- OWL_Ready/owl_manager.py
+++ OWL_Ready/owl_manager.py
@@ -1,18 +1,32 @@
-from owlready import *
+from owlready2 import *
 import os.path
 
 root_path = os.path.normpath(os.getcwd() + os.sep + os.pardir)
 os.chdir(root_path)
 
 onto_path.append(root_path)
-print(root_path)
-onto = get_ontology('file:../MobileOX.owl').load()
-# onto = get_ontology('https://www.gsmarena.com/ontologies/MobileOX.owl')
+onto = get_ontology('file:../Mobile_V1.3.owl').load()
+# onto = get_ontology('https://www.gsmarena.com/ontologies/Mobile_V1.3.owl')
 
-print("No of classes :", len(onto.classes))
+classes = list(())
+properties = list(())
+annot_properties = list(())
 
+for item in onto.classes():
+    classes.append(item)
+for item in onto.properties():
+    properties.append(item)
+for item in onto.annotation_properties():
+    annot_properties.append(item)
+
+print(classes)
+print("No of classes :", len(classes))
 print("Base URI :", onto.base_iri)
 print("Instances :", onto.instances)
-print("Properties :", onto.properties)
-print("Annotation properties :", onto.annotation_properties)
+print("Properties :", properties)
+print("Annotation properties :", annot_properties)
 print("Disjoints :", onto.all_disjoints)
+
+
+print(onto.search(hasPrice = '*'))
+print(onto.search(iri = "*TouchScreen"))
