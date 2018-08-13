719e9a11f52c40674bd559cf47d726929952838c
ArshadFauzil <mohomedarshad1@gmail.com>
2018-08-13 11:06:50 +0530

    added all queries

--- OWL_Ready/owl_manager.py
+++ OWL_Ready/owl_manager.py
@@ -31,15 +31,16 @@ for item in onto.annotation_properties():
 # print(onto.search(hasPrice = 30000))
 # print(onto.search(iri = "*TouchScreen"))
 
-sync_reasoner(my_world)
+# sync_reasoner(my_world)
 graph = my_world.as_rdflib_graph()
 
-
+# find users who have phones that cost more than the given price
 def findUsersByPrice(price):
     r = list(graph.query("""
-                SELECT DISTINCT ?s ?p
+                SELECT ?u ?p
                 WHERE {
-                ?s <https://www.gsmarena.com/ontologies/mobile.owl#hasPrice> ?p
+                ?s <https://www.gsmarena.com/ontologies/mobile.owl#hasPrice> ?p.
+                ?u <https://www.gsmarena.com/ontologies/mobile.owl#uses> ?s.
                 }"""))
     r = np.asarray(r)
     users_list = list(())
@@ -50,7 +51,7 @@ def findUsersByPrice(price):
     print(users_list)
     return users_list
 
-
+# recommend phones to users based on brand and OS
 def recommendPhone(brand, os):
     uri = '<https://www.gsmarena.com/ontologies/mobile.owl#'
     r = list(graph.query("""PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
@@ -70,10 +71,34 @@ def recommendPhone(brand, os):
         recommend.append(np.char.split(i, sep = '#')[0][1])
 
     recommend = np.asarray(recommend)
+    print(recommend)
+    return recommend
+
+# recommend phones based on chipset
+def RecommendOnChipset(chipset):
+    uri = '<https://www.gsmarena.com/ontologies/mobile.owl#'
+    r = list(graph.query("""PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+                            PREFIX owl:<http://www.w3.org/2002/07/owl#>
+                            SELECT ?model
+                            WHERE {
+                            ?model rdfs:subClassOf <https://www.gsmarena.com/ontologies/mobile.owl#SmartPhone>.
+                            ?mi a ?model.
+                            ?b a """ + uri + chipset + """>.
+                            ?mi <https://www.gsmarena.com/ontologies/mobile.owl#hasChipset> ?b.
+                            }"""))
+    r = np.asarray(r)
+    recommend = []
+    for i in r:
+        recommend.append(np.char.split(i, sep = '#')[0][1])
+
+    recommend = np.asarray(recommend)
+    print(recommend)
     return recommend
 
 
-br = 'Huawei'
+br = 'Motorola'
 oss = 'Oreo'
 # recommendPhone(br, oss)
-findUsersByPrice(30000)
+# findUsersByPrice(30000)
+chip = 'Qualcomm_SDM845_Snapdragon_845'
+# RecommendOnChipset(chip)
--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask_table import Table, Col
 from OWL_Ready.owl_manager import *
 
 app = Flask(__name__)
 
 app.secret_key = "AS9UjjJI0J0JS9j"
-
 
 @app.route('/')
 def home():
@@ -21,7 +21,7 @@ def search():
     try:
         phone_price = request.form['price']
 
-        users = findUsersByPrice(int(phone_price))
+        users = findUsersByPrice(phone_price)
         return render_template('found_wim_phone.html', ph_price=phone_price, users_list=users)
     except:
         return render_template('wim_phone.html')
@@ -33,8 +33,8 @@ def search1():
         user_name = request.form['username']
         brand_name = request.form['brandname']
         os = request.form['os']
-
-        return render_template('result1.html', name=user_name)
+        phones = recommendPhone(brand_name, os)
+        return render_template('result1.html', name=phones)
     except:
         return render_template('input_form1.html')
 
@@ -51,35 +51,10 @@ def search2():
         return render_template('input_form2.html')
 
 
-@app.route('/query3', methods=['GET'])
-def search3():
-    try:
-        user_name = request.form['username']
-        brand_name = request.form['brandname']
-        os = request.form['os']
-
-        return render_template('result3.html', name=user_name)
-    except:
-        return render_template('input_form3.html')
-
-
-@app.route('/query4', methods=['GET'])
-def search4():
-    try:
-        user_name = request.form['username']
-        brand_name = request.form['brandname']
-        os = request.form['os']
-
-        return render_template('result4.html', name=user_name)
-    except:
-        return render_template('input_form4.html')
-
 
 app.add_url_rule('/find_users', 'search', search, methods=['GET', 'POST'])
 app.add_url_rule('/query1', 'search1', search1, methods=['GET', 'POST'])
 app.add_url_rule('/query2', 'search2', search2, methods=['GET', 'POST'])
-app.add_url_rule('/query3', 'search3', search3, methods=['GET', 'POST'])
-app.add_url_rule('/query4', 'search4', search4, methods=['GET', 'POST'])
 
 if __name__ == "__main__":
     app.run(host='localhost', port=9800)
